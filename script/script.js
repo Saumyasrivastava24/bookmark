bfb87eac81a046eb4c5e8261095ffcf3d0681345
Saumyasrivastava24 <saumyasrivastav65@gmail.com>
2023-11-06 18:54:37 +0530

    bookmark completed

---
+++ script/script.js
@@ -0,0 +1,130 @@
+let google = document.getElementById('google');
+let youtube = document.getElementById('youtube');
+let friends = document.getElementById('friends');
+let github = document.getElementById('github');
+let gmail = document.getElementById('gmail');
+let chatgpt = document.getElementById('chatgpt');
+let bingewatch = document.getElementById('bingewatch');
+let instagram = document.getElementById('instagram'); 
+let telegram = document.getElementById('telegram'); 
+let btn = document.getElementById('btn');
+let id = [];
+google.addEventListener('click', () => {
+  google.style.background = 'rgb(10, 147, 128)';
+  id.unshift('https://www.google.com');
+  youtube.style.background =  'rgb(205 227 225 / 78%)';
+  friends.style.background =  'rgb(205 227 225 / 78%)';
+  github.style.background =  'rgb(205 227 225 / 78%)';
+  gmail.style.background =  'rgb(205 227 225 / 78%)';
+  chatgpt.style.background =  'rgb(205 227 225 / 78%)';
+  bingewatch.style.background =  'rgb(205 227 225 / 78%)';
+  instagram.style.background =  'rgb(205 227 225 / 78%)';
+  telegram.style.background =  'rgb(205 227 225 / 78%)';
+  
+});
+
+youtube.addEventListener('click', () => {
+   youtube.style.background = 'rgb(10, 147, 128)';
+  id.unshift('https://www.youtube.com');
+    google.style.background = 'rgb(205 227 225 / 78%)';
+  friends.style.background = 'rgb(205 227 225 / 78%)';
+  github.style.background = 'rgb(205 227 225 / 78%)';
+  gmail.style.background = 'rgb(205 227 225 / 78%)';
+  chatgpt.style.background = 'rgb(205 227 225 / 78%)';
+  bingewatch.style.background = 'rgb(205 227 225 / 78%)';
+  instagram.style.background = 'rgb(205 227 225 / 78%)';
+  telegram.style.background = 'rgb(205 227 225 / 78%)';
+});
+
+friends.addEventListener('click', () => {
+   friends.style.background = 'rgb(10, 147, 128)';
+  id.unshift('https://friends.saumya.live/'); 
+  google.style.background ='rgb(205 227 225 / 78%)';
+  youtube.style.background ='rgb(205 227 225 / 78%)';
+  github.style.background ='rgb(205 227 225 / 78%)';
+  gmail.style.background ='rgb(205 227 225 / 78%)';
+  chatgpt.style.background ='rgb(205 227 225 / 78%)';
+  bingewatch.style.background ='rgb(205 227 225 / 78%)';
+  instagram.style.background ='rgb(205 227 225 / 78%)';
+  telegram.style.background ='rgb(205 227 225 / 78%)';
+});
+
+github.addEventListener('click', () => {
+   github.style.background = 'rgb(10, 147, 128)';
+  id.unshift('https://github.com/Saumyasrivastava24');
+  google.style.background = 'rgb(205 227 225 / 78%)';
+  youtube.style.background = 'rgb(205 227 225 / 78%)';
+  friends.style.background = 'rgb(205 227 225 / 78%)';
+  gmail.style.background = 'rgb(205 227 225 / 78%)';
+  chatgpt.style.background = 'rgb(205 227 225 / 78%)';
+  bingewatch.style.background = 'rgb(205 227 225 / 78%)';
+  instagram.style.background = 'rgb(205 227 225 / 78%)';
+  telegram.style.background ='rgb(205 227 225 / 78%)';
+});
+gmail.addEventListener('click', () => {
+   gmail.style.background ='rgb(10, 147, 128)';
+  id.unshift('https://mail.google.com/');
+    google.style.background = 'rgb(205 227 225 / 78%)';
+  youtube.style.background = 'rgb(205 227 225 / 78%)';
+  friends.style.background = 'rgb(205 227 225 / 78%)';
+  github.style.background = 'rgb(205 227 225 / 78%)';
+  chatgpt.style.background = 'rgb(205 227 225 / 78%)';
+  bingewatch.style.background = 'rgb(205 227 225 / 78%)';
+  instagram.style.background = 'rgb(205 227 225 / 78%)';
+  telegram.style.background = 'rgb(205 227 225 / 78%)';
+   
+});
+chatgpt.addEventListener('click', () => {
+   chatgpt.style.background = 'rgb(10, 147, 128)';
+  id.unshift('https://chat.openai.com/');
+  google.style.background= 'rgb(205 227 225 / 78%)';
+  youtube.style.background= 'rgb(205 227 225 / 78%)';
+  friends.style.background= 'rgb(205 227 225 / 78%)';
+  github.style.background= 'rgb(205 227 225 / 78%)';
+  gmail.style.background= 'rgb(205 227 225 / 78%)';
+  bingewatch.style.background= 'rgb(205 227 225 / 78%)';
+  instagram.style.background= 'rgb(205 227 225 / 78%)';
+  telegram.style.background= 'rgb(205 227 225 / 78%)';
+});
+bingewatch.addEventListener('click', () => {
+   bingewatch.style.background = 'rgb(10, 147, 128)';
+  id.unshift('https://bingewatch.to/');
+   google.style.background = 'rgb(205 227 225 / 78%)';
+  youtube.style.background = 'rgb(205 227 225 / 78%)';
+  friends.style.background ='rgb(205 227 225 / 78%)';
+  github.style.background = 'rgb(205 227 225 / 78%)';
+  gmail.style.background = 'rgb(205 227 225 / 78%)';
+  chatgpt.style.background = 'rgb(205 227 225 / 78%)';
+  instagram.style.background = 'rgb(205 227 225 / 78%)';
+  telegram.style.background = 'rgb(205 227 225 / 78%)';
+});
+instagram.addEventListener('click', () => {
+   instagram.style.background = 'rgb(10, 147, 128)';
+  id.unshift('https://www.instagram.com/');
+  google.style.background = 'rgb(205 227 225 / 78%)';
+  youtube.style.background = 'rgb(205 227 225 / 78%)';
+  friends.style.background = 'rgb(205 227 225 / 78%)';
+  github.style.background = 'rgb(205 227 225 / 78%)';
+  gmail.style.background = 'rgb(205 227 225 / 78%)';
+  chatgpt.style.background = 'rgb(205 227 225 / 78%)';
+  bingewatch.style.background = 'rgb(205 227 225 / 78%)';
+  telegram.style.background = 'rgb(205 227 225 / 78%)';
+});
+telegram.addEventListener('click', () => {
+   telegram.style.background = 'rgb(10, 147, 128)';
+  id.unshift('https://web.telegram.org/a/');
+  google.style.background =  'rgb(205 227 225 / 78%)';
+  youtube.style.background = 'rgb(205 227 225 / 78%)';
+  friends.style.background = 'rgb(205 227 225 / 78%)';
+  github.style.background = 'rgb(205 227 225 / 78%)';
+  gmail.style.background = 'rgb(205 227 225 / 78%)';
+  chatgpt.style.background = 'rgb(205 227 225 / 78%)';
+  bingewatch.style.background = 'rgb(205 227 225 / 78%)';
+  instagram.style.background = 'rgb(205 227 225 / 78%)';
+});
+
+btn.addEventListener('click', () => {
+  if (id.length > 0) {
+    window.open(id[0]);
+  }
+});
